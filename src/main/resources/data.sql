a510aa249167405478e8559665ab44265b21a765
Jeff Lin <jefflin910424@gmail.com>
2024-07-30 15:56:31 +0800

    Added more data in H2

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,1 +1,3 @@
 insert into employee(id,name,password) values(2400681, 'Ren Lin','Foo123!');
+insert into employee(id,name,password) values(2400671, 'Joe Biden','123Foo!');
+insert into employee(id,name,password) values(3500681, 'Amy Chen','doggo24');
